3965d55a7abd2551a5709610b7563a9488f50cfc
MarkGocela <markloujames.gocela@devpartners.co>
2020-01-22 13:50:55 +0800

    add for website  ERA app

--- src/app/navigation/navigation.model.ts
+++
@@ -1,9 +0,0 @@
-import {FuseNavigationModelInterface} from '../core/components/navigation/navigation.model';
-
-export class FuseNavigationModel implements FuseNavigationModelInterface {
-  public model: any[];
-
-  constructor() {
-    this.model = [];
-  }
-}
--- src/app/notification/notification.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-import {NotificationListComponent} from './components/notification-list/notification-list.component';
-
-export const routes: Routes = [
-  {
-    path: 'list',
-    component: NotificationListComponent
-  }
-];
